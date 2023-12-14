d5762d35be8f199dcc2fa43b0d9cb3ea9a93c762
Marta <117610922+MartaMcx@users.noreply.github.com>
2023-12-14 12:25:38 +0100

    Delete Paradise directory

--- Paradise/Assets/_MAIN/Scrips/core/Dialogue/Commands/Database/CMD_DatabaseExtension.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class CMD_DatabaseExtension
-{
-    public static void Extend(CommandDatabase database) { }
-}
